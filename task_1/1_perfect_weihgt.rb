5d8fc69430df251751dd0b51ae33517a8630cf1f
Eugeny Vaulin <mactep.black@gmail.com>
2019-11-25 09:42:47 +0300

    Update 1_perfect_weihgt.rb

--- task_1/1_perfect_weihgt.rb
+++ task_1/1_perfect_weihgt.rb
@@ -3,7 +3,7 @@ name =  gets.chomp
 puts "Каков ваш рост в сантиметрах?"
 perfect_weihgt = gets.chomp.to_i - 110
 
-if perfect_weihgt > 0 then
+if perfect_weihgt > 0
   puts "#{name}, Ваш идеальный вес #{perfect_weihgt} кг."
 else
   puts "#{name}, Ваш вес уже оптимальный."
