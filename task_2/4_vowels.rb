096865066d7a10d9e6c614799bc0ad972c03a6bd
Eugeny Vaulin <mactep.black@gmail.com>
2019-11-26 09:25:49 +0300

    Update 4_vowels.rb

--- task_2/4_vowels.rb
+++ task_2/4_vowels.rb
@@ -1,6 +1,4 @@
 vowel_arr = %w( a e i o u )
 vowel_hash = {}
 
-("a".."z").each.with_index do |letter, index|
-  vowel_hash[letter] = index + 1 if vowel_arr.include?(letter)
-end 
+("a".."z").each.with_index(1) { |letter, index| vowel_hash[letter] = index if vowel_arr.include?(letter) }
